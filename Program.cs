04133c43b4a36741e9b1d8cc0650b930608de3cc
zhanggang99 <zhanggang@outlook.com>
2017-01-19 16:23:03 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace WebApiFrame
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder().UseKestrel().UseStartup<Startup>().Build();
+            host.Run();
+        }
+    }
+}
