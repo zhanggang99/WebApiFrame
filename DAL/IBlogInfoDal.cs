78163021e3285e34afbf778a0d5067a2deff0fc6
zhanggang99 <zhanggang@outlook.com>
2017-01-20 09:34:42 +0800

    添加 bloginfo model

---
+++ DAL/IBlogInfoDal.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using WebApiFrame.Models;
+namespace WebApiFrame{
+
+    public interface IBlogInfoDal{
+        
+    }
+
+}
---
+++ Models/BlogInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using Nest;
+namespace WebApiFrame{
+
+    public long BlogId{get;set;}
+    public string Title{get;set;}
+    public string Content{get;set;}
+    public DateTime CreatedTime{get;set;}
+    public int ClickCount{get;set;}
+
+}
