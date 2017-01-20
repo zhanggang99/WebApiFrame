57262b3e67bc8431600f3b55bf64d903e1df0f27
zhanggang99 <zhanggang@outlook.com>
2017-01-20 16:55:30 +0800

    init

---
+++ DAL/BlogInfoSQLDal.cs
@@ -0,0 +1,13 @@
+
+using System.Collections.Generic;
+using WebApiFrame.Models;
+namespace WebApiFrame{
+
+    public class BlogInfoSQLDal:IBlogInfoDal{
+    
+        public List<BlogInfo> GetBlogListByPage(int start,int end){
+            SqlConnection conn = new SqlConnection();
+            return null;
+        }
+    }
+}
--- Models/BlogInfo.cs
+++ Models/BlogInfo.cs
@@ -1,11 +1,14 @@
 using System;
-using Nest;
+
 namespace WebApiFrame{
 
+public class BlogInfo{
     public long BlogId{get;set;}
     public string Title{get;set;}
     public string Content{get;set;}
     public DateTime CreatedTime{get;set;}
     public int ClickCount{get;set;}
-
+    
+}
+    
 }
