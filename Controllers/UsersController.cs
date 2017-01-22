93f13aae0d0d0f0cc35d32800be5c106b6af78ae
zhanggang99 <zhanggang@outlook.com>
2017-01-22 09:09:02 +0800

    完善mcv功能

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -6,8 +6,9 @@ namespace WebApiFrame.Controllers{
 
 
     [Route("api/[controller]")]
-    public class UsersController:Microsoft.AspNetCore.Mvc.Controller{
-        [RouteAttribute("{id}")]
+    public class UsersController:Microsoft.AspNetCore.Mvc.Controller
+    {
+        [HttpGetAttribute("{id}")]
         public IActionResult Get(int id){
             var user = new User();
             user.Id=id;
@@ -15,6 +16,30 @@ namespace WebApiFrame.Controllers{
             user.Sex="male";
             return new ObjectResult(user);
         }
-        //http://www.cnblogs.com/niklai/p/5658876.html
+        [HttpPostAttribute]
+        public IActionResult Post([FromBody] User user){
+            if(user == null){
+                return BadRequest();
+            }
+            //todo：新增操作
+            user.Id = new Random().Next(1,10);
+            return CreatedAtAction("Get",new {id = user.Id},user);
+        }
+
+        [HttpPutAttribute("{id}")]
+        public IActionResult Put(int id,[FromBodyAttribute] User user){
+            if(user==null){
+                return BadRequest();
+            }
+            //todo:更新操作
+            return new NoContentResult();
+        }
+
+        [HttpDeleteAttribute("{id}")]
+        public void Delete(int id){
+            //todo:删除操作
+
+        }
+
     }
 }
